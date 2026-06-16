d2b7055fe8f128b9091f4d552e8f08bd7cfc3c9e
Daniel <ddbaby2004@gmail.com>
2026-06-16 14:47:31 -0300

    ajustes finos

--- alu.v
+++ alu.v
@@ -1,35 +1,34 @@
 module alu (
-    input wire [31:0] operando_a,   
-    input wire [31:0] operando_b,   
+    input wire [31:0] readData_1,   
+    input wire [31:0] readData_2,   
     input wire [3:0]  alu_control,  
     
-    output reg [31:0] resultado,    
+    output reg [31:0] alu_result,    
     output wire       zero          
 );
 
     always @(*) begin
         case (alu_control)
-            4'b0000: resultado = operando_a & operando_b; // AND 
-            4'b0001: resultado = operando_a | operando_b; // OR 
-            4'b0010: resultado = operando_a + operando_b; // SOMA 
-            4'b0110: resultado = operando_a - operando_b; // SUBTRAÇÃO 
-            4'b0011: resultado = operando_a ^ operando_b; // XOR (Ou exclusivo)
-            // Para deslocamentos no RISC-V32I, usamos apenas os 5 bits menos significativos do operando B
-            4'b0100: resultado = operando_a << operando_b[4:0]; // SLL
-            4'b0101: resultado = operando_a >> operando_b[4:0]; // SRL
+            4'b0000: alu_result = readData_1 & readData_2; // AND 
+            4'b0001: alu_result = readData_1 | readData_2; // OR 
+            4'b0010: alu_result = readData_1 + readData_2; // SOMA 
+            4'b0110: alu_result = readData_1 - readData_2; // SUBTRAÇÃO 
+            4'b0011: alu_result = readData_1 ^ readData_2; // XOR (Ou exclusivo)
+            4'b0100: alu_result = readData_1 << readData_2[4:0]; // SLL
+            4'b0101: alu_result = readData_1 >> readData_2[4:0]; // SRL
 				
             // SLT (Set on Less Than)
             4'b0111: begin
-                if ($signed(operando_a) < $signed(operando_b))
-                    resultado = 32'b1;
+                if ($signed(readData_1) < $signed(readData_2))
+                    alu_result = 32'b1;
                 else
-                    resultado = 32'b0;
+                    alu_result = 32'b0;
             end
             
-            default: resultado = 32'b0; 
+            default: alu_result = 32'b0; 
         endcase
     end
 
-    assign zero = (resultado == 32'b0) ? 1'b1 : 1'b0;
+    assign zero = (alu_result == 32'b0) ? 1'b1 : 1'b0;
 
 endmodule
